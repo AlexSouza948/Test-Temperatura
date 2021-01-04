951b26979bf67d0b32a0df91b32a486bc530c6f1
Alex Sandro <Alex Sandro@LAPTOP-8AS1G3AU>
2021-01-04 11:00:59 -0300

    TesteTDD

--- Temperatura/ConversorTemperatura.cs
+++ Temperatura/ConversorTemperatura.cs
@@ -7,6 +7,7 @@ namespace Temperatura
         public static double FahrenheitParaCelsius(double temperatura)
         {
             return Math.Round((temperatura - 32) / 1.8, 2);
+            //
         }
     }
 }
